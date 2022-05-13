fdde7f0ac7dc029e822c9831df45532aaad6529f
Leandro Guiñazú <87284449+LeandroGui@users.noreply.github.com>
2022-05-13 18:19:14 -0300

    Update Ejercicio28.java

--- Ejercicio28.java
+++ Ejercicio28.java
@@ -1,11 +1,12 @@
 /*
- * Dadas dos matrices cuadradas de n˙meros enteros, la matriz M de 10x10 y la matriz
- * P de 3x3, se solicita escribir un programa en el cual se compruebe si la matriz P est·
+ * Dadas dos matrices cuadradas de n√∫meros enteros, la matriz M de 10x10 y la matriz
+ * P de 3x3, se solicita escribir un programa en el cual se compruebe si la matriz P est√°
  * contenida dentro de la matriz M. Para ello se debe verificar si entre todas las
- * submatrices de 3x3 que se pueden formar en la matriz M, desplaz·ndose por filas o
+ * submatrices de 3x3 que se pueden formar en la matriz M, desplaz√°ndose por filas o
  * columnas, existe al menos una que coincida con la matriz P. En ese caso, el
  * programa debe indicar la fila y la columna de la matriz M en la cual empieza el
  * primer elemento de la submatriz P.
+ */
 
 
 package ejercicios;
